381d050b21c74ea385deeca8f2c701d110115b23
David Zimmermann <zimmy87@gmail.com>
2026-01-14 17:18:43 -0800

    add audit xtask

---
+++ xtask/src/audit.rs
@@ -0,0 +1,36 @@
+// Copyright (C) Microsoft Corporation. All rights reserved.
+
+#![warn(missing_docs)]
+#![forbid(unsafe_code)]
+
+//! Xtask to run various repo-specific audit checks
+
+use clap::Parser;
+use xshell::cmd;
+
+use crate::Xtask;
+use crate::XtaskCtx;
+
+/// Xtask to run various repo-specific audit checks
+#[derive(Parser)]
+#[clap(about = "Run various audit checks")]
+pub struct Audit {}
+
+impl Xtask for Audit {
+    fn run(self, _ctx: XtaskCtx) -> anyhow::Result<()> {
+        log::trace!("running audit");
+
+        let sh = xshell::Shell::new()?;
+        let rust_toolchain = sh.var("RUST_TOOLCHAIN").map(|s| format!("+{s}")).ok();
+
+        cmd!(
+            sh,
+            "cargo {rust_toolchain...} audit"
+        )
+        .quiet()
+        .run()?;
+
+        log::trace!("done audit");
+        Ok(())
+    }
+}
