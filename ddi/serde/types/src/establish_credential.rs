871ed1b680c5f132ed40d4d11dad7ea97863c90c
Jayant Gandhi <jayg@microsoft.com>
2026-02-04 19:40:44 +0000

    Update serde

--- ddi/serde/types/src/establish_credential.rs
+++ ddi/serde/types/src/establish_credential.rs
@@ -31,11 +31,47 @@ pub struct DdiEstablishCredentialReq {
 
     /// Partition ID signed by the TPM
     #[ddi(id = 6)]
+    #[ddi(pre_encode_fn = "signature_pre_encode")]
     pub signed_pid: MborByteArray<1024>,
 
     /// TPM Public Key
     #[ddi(id = 7)]
     pub tpm_pub_key: DdiDerPublicKey,
+}
+
+impl DdiEstablishCredentialReq {
+    #[cfg(feature = "pre_encode")]
+    pub fn signature_pre_encode(
+        &self,
+        input_array: &MborByteArray<1024>,
+    ) -> Result<MborByteArray<1024>, MborEncodeError> {
+        let mut output_array = [0u8; 1024];
+
+        // We need to split the array into two components,
+        // then convert endianness for each component.
+
+        // Validate length.
+        if input_array.len() != 96 {
+            Err(MborEncodeError::InvalidLen)?
+        }
+
+        let input_component_len = input_array.len() / 2;
+
+        // Convert from big Endian to little Endian for first component.
+        let data = input_array.data();
+        reverse_copy(
+            &mut output_array[..input_component_len],
+            &data[..input_component_len],
+        );
+
+        // Convert from little Endian to big Endian for second component.
+        reverse_copy(
+            &mut output_array[input_component_len..input_array.len()],
+            &data[input_component_len..input_array.len()],
+        );
+
+        Ok(MborByteArray::new(output_array, input_array.len())?)
+    }
 }
 
 /// DDI Get Param Encryption Key Response Structure
