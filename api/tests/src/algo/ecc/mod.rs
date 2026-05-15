2d375100e4bebacc1cf84b641366cadd51fca157
Mei Lu <mei.lu@businessintegra.com>
2026-05-14 22:14:07 -0700

    CR comments

--- api/tests/src/algo/ecc/mod.rs
+++ api/tests/src/algo/ecc/mod.rs
@@ -1,16 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+mod common;
 mod ecdh_tests;
 mod hash_sign_tests;
 mod key_prop_tests;
 mod key_tests;
-
 mod nist_tests;
 mod sign_tests;
 
 pub(crate) use ecdh_tests::*;
 
 use super::*;
-
-pub mod common;
