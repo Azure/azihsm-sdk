7e181b82edce2aa3b07b11a93a8f6d2f10854d18
David Zimmermann <zimmy87@gmail.com>
2026-08-27 16:51:13 -0700

    add tbor cert fuzz targets

---
+++ fuzz/fuzz_targets/ddi/tbor/fuzz_tbor_get_cert.rs
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#![no_main]
+
+#[path = "../../common.rs"]
+mod common;
+
+use azihsm_ddi_tbor_types::TborGetCertReq;
+use libfuzzer_sys::arbitrary;
+use libfuzzer_sys::arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
+
+#[derive(Arbitrary, Debug)]
+struct FuzzInput {
+    slot_id: u8,
+    cert_id: u8,
+}
+
+fuzz_target!(|input: FuzzInput| {
+    let dev = common::open_emu_dev();
+    let req = TborGetCertReq {
+        slot_id: input.slot_id,
+        cert_id: input.cert_id,
+    };
+    common::fuzz_exec_op_tbor(&dev, &req);
+});
